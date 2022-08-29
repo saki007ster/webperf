f853f8550eb5a0ad2e6dcb9674dc126e9ddaa9dd
Saket Kumar <saki007ster@gmail.com>
2022-08-29 11:50:21 -0500

    first commit

---
+++ _data/sites/qed42_projects.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "qed42 projects",
+  description: "Qed42 build sites",
+  options: {
+    frequency: 6,
+    freshChrome: "run",
+  },
+  urls: [
+    "https://www.qed42.com/",
+    "https://www.qed42.design/",
+    "https://hey.qed42.com/",
+    "https://space-launch.netlify.app/",
+    "https://www.ashoka.org/en-us",
+    "https://www.nestle.com/",
+    "https://www.gsb.stanford.edu/",
+    "https://www.here.com/",
+    "https://www.patrika.com/",
+    "https://www.barschool.net",
+    "https://www.shopthearea.net/",
+    "https://www.kotaksecurities.com/",
+  ],
+};
